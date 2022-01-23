5da40f1ddc3b40e63ab7cd726e2ba8fb1524292a
khaledghaffarii <med.khaledghaffary@gmail.com>
2022-01-23 23:27:26 +0100

    upadate the cv model

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 // import Particle from "../Particle";
-import pdf from "../../Assets/CV_2022-01-21_Mohemed Khaled_Ghaffari.pdf";
+import pdf from "../../Assets/CV_2022-01-23_Mohemed Khaled_Ghaffari.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/khaledghaffarii/portfolio_2.0/master/src/Assets/CV_2022-01-21_Mohemed Khaled_Ghaffari.pdf";
+  "https://raw.githubusercontent.com/khaledghaffarii/portfolio_2.0/master/src/Assets/CV_2022-01-23_Mohemed Khaled_Ghaffari.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
